f2ae3bff36c244d632177af55cbff678f0e31b00
Nicholas Crothers <hawx523@gmail.com>
2024-08-22 16:42:15 -0500

    Start work on preprocessing steps before parsing

--- flatbuffers-codegen/src/parser.rs
+++ flatbuffers-codegen/src/parser.rs
@@ -5,6 +5,15 @@ use winnow::{
 
 use crate::flatbuffers::item::{item, Item};
 
+pub fn collect_includes(file: &str) -> Vec<&str> {
+    let pat = regex::Regex::new(r#"^[ \t]*include[ \t\n]+"([\.\w]+\.fbs)""#).unwrap();
+
+    pat.captures_iter(file)
+        .flat_map(|val| val.iter().skip(1).collect::<Vec<_>>())
+        .filter_map(|val| val.map(|x| x.as_str()))
+        .collect()
+}
+
 pub fn parse_file(file: &str) -> anyhow::Result<Vec<Item>> {
     trace("parse_file", repeat(0.., item))
         .parse(file)
